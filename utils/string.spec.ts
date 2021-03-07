faaecf7a2c252ccaa6111499ad5713e1e5f66325
Satoshi Onoda <satoshionoda@gmail.com>
2021-03-07 10:12:37 +0900

    undefined 対策

--- utils/string.spec.ts
+++ utils/string.spec.ts
@@ -8,4 +8,8 @@ describe("unescapeJsonString", () => {
     // console.log(result);
     expect(result).toBe('"Pediococcus lindneri" Henneberg 1926');
   });
+  it("should not stop even if undefined is passed", () => {
+    const result = unescapeJsonString(undefined);
+    expect(result).toBe(undefined);
+  });
 });
--- utils/string.ts
+++ utils/string.ts
@@ -1,3 +1,3 @@
 export const unescapeJsonString = (str: string): string => {
-  return str.replace(/\\/g, "");
+  return str?.replace(/\\/g, "");
 };
