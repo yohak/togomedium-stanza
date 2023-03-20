73cf89260fcc45a336ca8d8197c03c55529bfd5b
satoshionoda <satoshionoda@gmail.com>
2023-03-20 12:28:44 +0900

    ビルド速度チューニング

--- api/all-components/msw.ts
+++ api/all-components/msw.ts
@@ -1,8 +1,8 @@
 import { rest } from "msw";
 import { allComponentsResponse } from "./response";
-import { API_ALL_COMPONENTS } from "../paths";
+import { API_COMPONENTS_WITH_COMPONENTS } from "../paths";
 
-const post = rest.post(API_ALL_COMPONENTS, (req, res, ctx) => {
+const post = rest.post(API_COMPONENTS_WITH_COMPONENTS, (req, res, ctx) => {
   return res(ctx.delay(1000), ctx.status(200), ctx.json(allComponentsResponse));
 });
 
