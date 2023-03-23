7bbca2e7d48bf0e9b6ff515711166b116aaf04cc
satoshionoda <satoshionoda@gmail.com>
2023-03-23 17:52:01 +0900

    Env情報を更新

--- shared/utils/variables.ts
+++ shared/utils/variables.ts
@@ -1,2 +1,2 @@
-const ENV_URL_API = typeof process !== "undefined" ? process.env?.URL_API : "";
+const ENV_URL_API = typeof process !== "undefined" ? process.env?.URL_API : undefined;
 export const URL_API: string = ENV_URL_API ?? "http://togomedium.org/sparqlist/api/";
