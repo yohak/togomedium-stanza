63f24a06ef900a8e590e5a450043fbe1ae6f5d59
Satoshi Onoda <satoshionoda@gmail.com>
2024-06-24 17:47:21 +0900

    wip: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/eslint-recommended",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
     "plugin:storybook/recommended",
   ],
   plugins: ["@typescript-eslint", "react", "react-hooks", "@emotion", "import"],
