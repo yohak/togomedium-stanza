3c1ba64f0fea37240fc92acec66ff44d47c3e39a
satoshionoda <satoshionoda@yohak.design>
2024-07-25 14:37:00 +0900

    wip: react-query

--- stanzas/gmdb-media-alignment-table/App.tsx
+++ stanzas/gmdb-media-alignment-table/App.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/react";
-import React, { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import React, { useCallback } from "react";
 import { ScrollableTable } from "./components/ScrollableTable";
 import { MediaAlignmentTableResponse } from "../../api/media-alignment-table/types";
 import { API_MEDIA_ALIGNMENT } from "../../api/paths";
@@ -12,36 +13,38 @@ export type AppProps = {
   stanzaElement?: ShadowRoot;
 };
 
-//TODO: switch to use react-query
-const App = ({ gm_ids, stanzaElement, prioritizedOrganism = [] }: AppProps) => {
-  const [data, setData] = useState<MediaAlignmentTableResponse>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const dispatchLoadData = (detail: any) => {
-    if (!stanzaElement) return;
-    stanzaElement.dispatchEvent(
-      new CustomEvent("STANZA_ON_LOAD_DATA", { bubbles: true, composed: true, detail })
-    );
-  };
-  const dispatchOnQueryData = (detail: string[]) => {
-    if (!stanzaElement) return;
-    stanzaElement.dispatchEvent(
-      new CustomEvent("STANZA_ON_QUERY_DATA", { bubbles: true, composed: true, detail })
-    );
-  };
-  useEffect(() => {
-    setData(undefined);
-    setIsLoading(true);
-    (async () => {
-      dispatchOnQueryData(gm_ids);
+const useDataQuery = (
+  gm_ids: string[],
+  stanzaDispatch: (eventName: string, detail: any) => void
+) => {
+  return useQuery({
+    queryKey: ["media-alignment", { gm_ids }],
+    queryFn: async () => {
+      stanzaDispatch("STANZA_ON_QUERY_DATA", gm_ids);
       const response = await getData<MediaAlignmentTableResponse>(API_MEDIA_ALIGNMENT, { gm_ids });
-      setIsLoading(false);
-      setData(response.body);
-      dispatchLoadData(response.body);
-    })();
-  }, [gm_ids]);
+      if (!response.body) throw new Error("No data");
+      stanzaDispatch("STANZA_ON_LOAD_DATA", response.body);
+      return response.body;
+    },
+    enabled: gm_ids.length > 0,
+    staleTime: Infinity,
+  });
+};
+
+const App = ({ gm_ids, stanzaElement, prioritizedOrganism = [] }: AppProps) => {
+  const dispatchStanzaEvent = useCallback(
+    (eventName: string, detail: any) => {
+      if (!stanzaElement) return;
+      const e = new CustomEvent(eventName, { bubbles: true, composed: true, detail });
+      stanzaElement.dispatchEvent(e);
+    },
+    [stanzaElement]
+  );
+  const dataQuery = useDataQuery(gm_ids, dispatchStanzaEvent);
+  if (!dataQuery.data) return <div css={wrapper}>Loading...</div>;
   return (
     <div css={wrapper}>
-      {data && <ScrollableTable data={data} prioritizedOrganism={prioritizedOrganism} />}
+      <ScrollableTable data={dataQuery.data} prioritizedOrganism={prioritizedOrganism} />
     </div>
   );
 };
